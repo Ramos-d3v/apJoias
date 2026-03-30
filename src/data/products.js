6f4a7fab6b3dea2112fff07cb426d41b058be92c
Enzo <enzopyton24@gmail.com>
2026-03-30 17:44:40 -0300

    Change images

--- src/data/products.js
+++ src/data/products.js
@@ -1,15 +1,18 @@
+import imagemAnel from "../assets/imagemTeste.jpg"
+import imagemColar from "../assets/imagemTeste2.jpg"
+
 export const products = [
   {
     id: 1,
     name: "Anel Solitário Diamante 18k",
     price: "R$ 4.500,00",
-    image: "https://images.unsplash.com/photo-1605100804763-247f66156ce4?auto=format&fit=crop&q=80&w=800",
+    image: imagemAnel,
   },
   {
     id: 2,
     name: "Colar Pérola e Ouro Branco",
     price: "R$ 2.850,00",
-    image: "https://images.unsplash.com/photo-1599643478524-fb66f70d00f6?auto=format&fit=crop&q=80&w=800",
+    image: imagemColar,
   },
   {
     id: 3,
